501a4256a0e0e6e42c29af3b7a5fc5a421b7bca8
Nicolas Derumigny <nicolas.derumigny@ens-lyon.fr>
2017-11-29 16:35:55 +0100

    Moving library folder and creation of files for parsing
    Fixing a mistake, and adding header and source file for parsing functions

---
+++ header/executable_options.h
@@ -0,0 +1,8 @@
+/** 
+ * \author Daumen Aanton and Nicolas Derumigny
+ * \file executable_options.h
+ * \brief Definition of the tools for the command-line options.
+ *
+ */
+
+
---
+++ src/executable_options.c
@@ -0,0 +1,8 @@
+/** 
+ * \author Daumen Aanton and Nicolas Derumigny
+ * \file executable_options.c
+ * \brief Definition of the tools for the command-line options.
+ *
+ */
+
+#include "executable_options.h"
